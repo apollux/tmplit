6efb07bafd72e7afb82fd8d883f5536e2c79e7b1
Andre Kwakernaak <andre.kwakernaak@gmail.com>
2012-08-02 15:01:28 +0200

    fill templates before writing output

--- generator.py
+++ generator.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+from Cheetah.Template import Template
+
 def ensure_dir_exists(path):
 	d = os.path.dirname(path)
 	if not os.path.exists(d):
@@ -13,10 +15,11 @@ for root, sub_folders, files in os.walk(template_dir):
 	for template_file in files:
 		path_to_template = os.path.join(root, template_file)
 		with open(path_to_template, 'r') as template:
-			t = template.read()
+			t = Template(file = template, searchList = [{'name':'andre'}])
+			filled = str(t)
 
 		output_path = os.path.join(output_dir, os.path.relpath(path_to_template, template_dir))
 		ensure_dir_exists(output_path)
 		with open(output_path, 'w+') as output:
-			output.write(t)
+			output.write(filled)
 
